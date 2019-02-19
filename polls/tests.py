0ab1bb8813b0d3f675724461bf6de6f3a915c087
Khuram <m.khuramj@live.com>
2019-02-19 17:42:09 +0500

    (Part-07)- Testing: with APIClient

--- polls/tests.py
+++ polls/tests.py
@@ -1,14 +1,14 @@
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-from rest_framework.test import APITestCase
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient, APITestCase, APIRequestFactory
 
 from polls import apiviews
 
 
 class TestPoll(APITestCase):
     def setUp(self):
+        self.client = APIClient()
         self.factory = APIRequestFactory()
         self.view = apiviews.PollViewSet.as_view({'get': 'list'})
         self.uri = '/polls/'
@@ -31,6 +31,13 @@ class TestPoll(APITestCase):
         request.user = self.user
         response = self.view(request)
 
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_list2(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code))
